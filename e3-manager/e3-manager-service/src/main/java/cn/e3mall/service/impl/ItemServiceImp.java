0e40bd64bd9bc92bb3ccda843920ed7771d1b89a
weihua yuan <itheima_test@sina.com>
2017-12-28 20:36:40 +0800

    添加manager

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImp.java
@@ -0,0 +1,21 @@
+package cn.e3mall.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+@Service
+public class ItemServiceImp implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	public TbItem getItemById(Long id) {
+		TbItem item = itemMapper.selectByPrimaryKey(id);
+		return item;
+	}
+
+}
